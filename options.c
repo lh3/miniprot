f076cbfec776538ffde29a330fdda4ea7a2cf08e
Heng Li <lh3@me.com>
2022-09-27 12:50:29 -0400

    r146: changed default --outn to 1000
    It doesn't hurt to have a large default value. Related to #13.

--- options.c
+++ options.c
@@ -39,7 +39,7 @@ void mp_mapopt_init(mp_mapopt_t *mo)
 	mo->mask_len = INT32_MAX;
 	mo->pri_ratio = 0.7f;
 	mo->best_n = 50;
-	mo->out_n = 100;
+	mo->out_n = 1000;
 	mo->kmer2 = 5;
 
 	mo->go = 11, mo->ge = 1;
