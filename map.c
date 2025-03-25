f740e111891cdf86f67c45d0c7e9c9a318ceb67c
Heng Li <lh3@me.com>
2025-03-25 10:47:49 -0400

    r268: improved bracket
    For bracketing alignment, we should use max_intron/2, not max_ext.

--- map.c
+++ map.c
@@ -224,7 +224,7 @@ mp_reg1_t *mp_map(const mp_idx_t *mi, int qlen, const char *seq, int *n_reg, mp_
 		int32_t k;
 		uint64_t *ext = 0;
 		if (opt->flag & MP_F_BRACKET_ALN)
-			ext = mp_cal_max_ext(km, mi->nt, *n_reg, reg, a, 100, opt->max_ext);
+			ext = mp_cal_max_ext(km, mi->nt, *n_reg, reg, a, 100, opt->max_intron/2); // opt->max_intron/2 for consistency with mp_align()
 		for (i = k = 0; i < *n_reg; ++i) {
 			if (ext)
 				mp_align(km, opt, mi, qlen, seq, &reg[i], ext[i]>>32, (int32_t)ext[i]);
--- miniprot.h
+++ miniprot.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define MP_VERSION "0.14-r267-dirty"
+#define MP_VERSION "0.14-r268-dirty"
 
 #define MP_F_NO_SPLICE    0x1
 #define MP_F_NO_ALIGN     0x2
